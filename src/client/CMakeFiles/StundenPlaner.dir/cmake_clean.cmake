af5ba77042d672faac0a59f68a55f872b0b0b08e
Vincent Woltmann <vincent.woltmann@web.de>
2016-08-28 13:55:38 +0200

    Restructuring

---
+++ src/client/CMakeFiles/StundenPlaner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StundenPlaner.dir/stunden_planer.cpp.o"
+  "StundenPlaner.pdb"
+  "StundenPlaner"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StundenPlaner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
